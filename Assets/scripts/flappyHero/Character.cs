9b02b4dcaefaa1a67a01018ac4959b67f3b1bb6a
Eric Moerth <n1325306@students.meduniwien.ac.at>
2018-11-20 09:02:53 +0100

    Initial Commit

---
+++ Assets/scripts/flappyHero/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float upForce = 100f;
+
+    public Rigidbody2D rb;
+    private bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();    
+	}
+
+    public void flap()
+    {    
+        rb.velocity = Vector2.zero;
+        rb.AddForce(new Vector2(0, upForce));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        rb.velocity = Vector2.zero;
+        GamesController.instance.BirdDied();
+    }
+
+}
---
+++ Assets/scripts/flappyHero/GamesController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamesController : MonoBehaviour {
+
+    public static GamesController instance;
+    public Character character;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+
+    private int score = 0;
+
+    // Used for initialization
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+	// Update is called once per frame
+	void Update () {
+	    if( Input.GetMouseButtonDown(0))
+        {
+            updateFunction();
+        }
+	}
+
+    // use same behaviour than with mouse press
+    public void androidFlap(string value)
+    {
+        updateFunction();
+    }
+
+    private void updateFunction()
+    {
+        if (gameOver == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            character.flap();
+        }
+    }
+
+    public void BirdScored()
+    {
+        if(gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+}
